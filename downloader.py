1a829d3c4b68a8d9e712b8d9b72827713b29d675
Tobias Tilly <tobias@tilly.io>
2017-05-03 02:34:28 +0200

    added argumentparsing and a full run on data #10

--- downloader.py
+++ downloader.py
@@ -13,14 +13,14 @@ from urllib2 import urlopen, URLError, HTTPError
 
 
 # source: http://stackoverflow.com/questions/4028697/how-do-i-download-a-zip-file-in-python-using-urllib2
-def download_file(url):
+def download_file(url, path, name):
     # Open the url
     try:
         f = urlopen(url)
         print "downloading " + url
 
         # Open our local file for writing
-        with open("data/"+os.path.basename(url), "wb") as local_file:
+        with open(path + name, "wb") as local_file:
             local_file.write(f.read())
 
     #handle errors
@@ -33,15 +33,12 @@ def download_file(url):
 def download_1k(chromosome):
     base_url = "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/"
     file_name = "ALL.chr{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz".format(chromosome)
-    download_file(base_url+file_name)
-    download_file(base_url+file_name+ '.tbi')
+    download_file(base_url+file_name, "data/", "1k_chr{}_phase3.vcf.gz".format(chromosome))
+    download_file(base_url+file_name+ '.tbi', "data/", "1k_chr{}_phase3.vcf.gz.tbi".format(chromosome))
 
 def download_1k_panel():
-    download_file("ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/integrated_call_samples_v3.20130502.ALL.panel")
+    download_file("ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/integrated_call_samples_v3.20130502.ALL.panel", "data/", "1k_phase3.panel")
 
 def download_gnomad(chromosome):
     url = "https://storage.googleapis.com/gnomad-public/release-170228/vcf/genomes/gnomad.genomes.r2.0.1.sites.{}.vcf.gz".format(chromosome)
-    download_file(url)
-
-
-download_1k(2)
+    download_file(url, "data/", "gnomad_chr{}.vcf.gz".format(chromosome))
--- main.py
+++ main.py
@@ -9,28 +9,23 @@ The main module holds all other modules and controls the workflow.
 
 ToDo:
 	this could be extended for a nice CLI (with docopt)
-	allow generation of statistic as well as reading it from file
 '''
+
+import argparse
+import sys
 
 import wrapper
 import parser
 import output
-
+import downloader
 
 # output-directories
 OUTPUT_BCFTOOLS = "bcftools-output/"
 OUTPUT_VCFTOOLS = "vcftools-output/"
 
-# vcf.gz paths
-PATH_1K_VCFGZ = "data/1k_phase3_chr22.vcf.gz"
-PATH_GNOMAD_VCFGZ = "data/gnomad_chr22.vcf.gz"
-PATH_1K_EUR_VCFGZ = "data/isec_EUR_Gnomad/0002.vcf"
-PATH_GNOMAD_EUR_VCFGZ = "data/isec_EUR_Gnomad/0003.vcf"
-
-
 def shallow_analysis(file_path):
     bcf_stats = wrapper.get_bcf_stats(file_path)
-    stat_summary = parser.analyze_bcf_stats(bcf_stats)
+    stat_summary = parser.analyze_bcf_stats(bcf_stats, file_path)
     output.print_statistic_summary(stat_summary)
 
 def create_intersection(files, output_dir):
@@ -51,20 +46,51 @@ def plot_af_stats():
 	with open(OUTPUT_BCFTOOLS + 'bcf_stats_gnomad_EUR', 'r') as f:
 		bcf_stats = f.read()
 		EUR_gnomad_af = parser.extract_allele_frequencies(bcf_stats)
-	output.plot_allele_frequencies(EUR_1k_af, '1000Genomes')
-	#output.plot_allele_frequency_comparison(EUR_1k_af, EUR_gnomad_af, ['1000Genomes','GnomAD'])
+	#output.plot_allele_frequencies(EUR_1k_af, '1000Genomes')
+	output.plot_allele_frequency_comparison(EUR_1k_af, EUR_gnomad_af, ['1000Genomes','GnomAD'])
 		
-def subset_filtered_list():
-	population_list = open("data/1k_all.panel", 'r')
-	eur_list = wrapper.filter_list(population_list, "EUR")
+def subset_filtered_list(file):
+	population_list = open('data/1k_phase3.panel', 'r')
+	eur_list = wrapper.filter_list(population_list, 'EUR')
 	eur_column = [x.split("\t")[0] for x in eur_list ]
 	nonFin_list = wrapper.filter_list(eur_list, "FIN", True)
 	nonFin_column = [x.split("\t")[0] for x in nonFin_list ]
-	wrapper.wrap_vcf_subset("data/1k_phase3_chr22.vcf.gz", eur_column, "eur_1k")
-	wrapper.wrap_vcf_subset("data/1k_phase3_chr22.vcf.gz", nonFin_column, "nonFin_1k")
+	wrapper.wrap_vcf_subset(file, eur_column, "data/eur_1k")
+	wrapper.wrap_vcf_subset(file, nonFin_column, "data/nonFin_1k")
+
+def initial_download(chromosome):
+	downloader.download_1k(chromosome)
+	downloader.download_1k_panel()
+	downloader.download_gnomad(chromosome)
 
 
 
-#shallow_analysis()
-#plot_af_stats()
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--download', 
+	help='starts downloading the 1k and gnomad files for the given chromosome')
+arg_parser.add_argument('--files', nargs='+', help='takes 2 files for the full analysis(first 1k, then gnomad)')
+arg_parser.add_argument('--summary', help='takes one file for a statistic summary')
 
+args = arg_parser.parse_args()
+
+if args.download:
+	initial_download(args.download)
+
+if args.summary:
+	shallow_analysis(args.summary)
+elif args.files:
+	if len(args.files) > 2:
+		print("Only 2 files are allowed. You gave: {}".format(len(args.files)))
+		sys.exit(1)
+	else:
+		for file in args.files:
+			shallow_analysis(file)
+		print("Creating EUR and nonFIN-EUR Subsets...")
+		subset_filtered_list(args.files[0])
+		print("Subsets created!")
+		print("Intersecting filtered 1k_EUR with gnomAD...")
+		create_intersection(["data/eur_1k_subset_output.vcf.gz", args.files[1]], "data/isec_eur")
+		print("EUR Intersection created!")
+		print("Creating EUR stats...")
+		create_EUR_stats("data/isec_eur/0002.vcf", "data/isec_eur/0003.vcf")
+		plot_af_stats()
